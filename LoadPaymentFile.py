b8ca8ab45cb49e4e8885c416ae92e657c6274460
Chengzhi Zhao <w.zhaochengzhi@gmail.com>
2017-05-05 10:22:46 +0800

    update key value

--- LoadPaymentFile.py
+++ LoadPaymentFile.py
@@ -11,7 +11,7 @@ with open ('Rule.txt','r') as inputfile:
     for line in inputfile.readlines()[1:]:
         RuleGroupID,RuleID,TypeID,FieldName = line.split('|')
         ruleID = (int(RuleGroupID),int(RuleID),int(TypeID))
-        r.set(ruleID,FieldName[:-1])
+        r.set('Rule:'+str(ruleID),FieldName[:-1])
 
 #Store Rule Relationship Data        
 with open ('RuleRelationship.txt','r') as inputfile:
@@ -20,11 +20,11 @@ with open ('RuleRelationship.txt','r') as inputfile:
         ruleID = (int(RuleGroupID),int(RuleID))
         nextRuleID = (int(0 if NextRuleGroupID == '' else NextRuleGroupID),int(0 if NextRuleID == '' else NextRuleID))
         ruleConcatenation = (Parenthesis,LogicalOperation)
-        r.set(ruleID, (nextRuleID,ruleConcatenation))
+        r.set('RuleRelationship:' + str(ruleID), (nextRuleID,ruleConcatenation))
         
 #Get some Data!!
 with open('Payment.txt','r') as inputfile:
     for line in inputfile.readlines()[1:]:
         invoiceNumber, name, amount = line.split('|')
         nameAmount = (name, float(amount[:-1]))
-        r.set(invoiceNumber, nameAmount)
+        r.set('Payment:' + invoiceNumber, nameAmount)
